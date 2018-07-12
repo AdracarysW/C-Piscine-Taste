4e88a09a29f30c91d857fa5b55651bc33813a45f
Amar Waraich <awaraich@e1z2r7p4.42.us.org>
2018-07-12 15:30:38 -0700

    Finish up to day 3 - Exam tomorrow

--- Day02/ex01/ft_print_reverse_alphabet.c
+++ Day02/ex01/ft_print_reverse_alphabet.c
@@ -1,35 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: awaraich <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/07/11 10:37:19 by awaraich          #+#    #+#             */
-/*   Updated: 2018/07/12 12:20:27 by awaraich         ###   ########.fr       */
+/*   Created: 2018/07/12 13:07:06 by awaraich          #+#    #+#             */
+/*   Updated: 2018/07/12 13:20:20 by awaraich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 
-void    ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
-void    ft_print_alphabet(void)
+void 	ft_print_reverse_alphabet(void)
 {
-    char ch = 'a';
-    while (ch <= 'z')
-    {
-        ft_putchar(ch);
-        ch++;
-    }
-}
-
-int     main(void)
-{
-    ft_print_alphabet();
-    return (0);
+	char ch = 'z';
+	while(ch >= 'a')
+	{
+		ft_putchar(ch);
+		ch--;
+	}
 }
