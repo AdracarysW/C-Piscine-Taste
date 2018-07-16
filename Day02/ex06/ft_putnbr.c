b4d9ea32023db9870699fea135907ca6c30dd9a9
Amar Waraich <amarwaraich@Amars-MBP.hsd1.ca.comcast.net>
2018-07-16 16:39:57 -0700

    Commit somethin'

---
+++ Day02/ex06/ft_putnbr.c
@@ -0,0 +1,20 @@
+int		ft_putchar(char c);
+
+void	ft_putnbr(int nb)
+{
+	char	c;
+
+	if (nb < 0)
+	{
+		nb = -nb;
+		ft_putchar('-');
+	}
+	if (nb < 10)
+		ft_putchar(nb + '0');
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
+
